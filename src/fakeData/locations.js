ee71db739afa8b7633421e8c8d8f83a1fc3a2e49
Md Salman <md.salman@northsouth.edu>
2020-09-24 21:44:09 +0600

    1

---
+++ src/fakeData/locations.js
@@ -0,0 +1,103 @@
+const locations = [
+	{
+		id: 1,
+		name: "COX's BAZAR",
+		description:
+			" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda officia a, error aperiam temporibus enim nihil vitae inventore consectetur placeat culpa reiciendis quia rem amet delectus doloremque odio doloribus modi esse optio vero laboriosam praesentium aspernatur. Officia architecto quasi inventore voluptatem sed error facere neque, ratione non optio consequatur. Mollitia. ",
+		image: "../resources/images/Sajek.png",
+		hotels: [
+			{
+				id: 1,
+				name: "Cox Hotel 1",
+				description:
+					" Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis nemo dolore quaerat nobis temporibus quo tempora porro illo voluptatum nesciunt! ",
+				image:
+					"../resources/images/Rectangle 26.png",
+			},
+			{
+				id: 2,
+				name: "Cox Hotel 2",
+				description:
+					" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate blanditiis nulla, corporis fugiat enim accusamus reiciendis voluptatum optio laboriosam perspiciatis? ",
+				image:
+					"../resources/images/Rectangle 27.png",
+			},
+			{
+				id: 3,
+				name: "Cox Hotel 3",
+				description:
+					" Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quo repellat laboriosam illum suscipit minima rem commodi officia expedita a? ",
+				image:
+					"../resources/images/Rectangle 28.png",
+			},
+		],
+	},
+	{
+		id: 2,
+		name: "SREEMANGAL",
+		description:
+			" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque nihil rem harum ipsa dignissimos hic expedita. Ipsam qui repudiandae veniam! ",
+		image: "../resources/images/Sreemongol.png",
+		hotels: [
+			{
+				id: 1,
+				name: "Sree hotel 1",
+				description:
+                " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quo repellat laboriosam illum suscipit minima rem commodi officia expedita a? ",
+				image:
+					"../resources/images/Rectangle 26.png",
+			},
+			{
+				id: 2,
+				name: "Sree hotel 2",
+				description:
+                " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quo repellat laboriosam illum suscipit minima rem commodi officia expedita a? ",
+				image:
+					"../resources/images/Rectangle 27.png",
+			},
+			{
+				id: 3,
+				name: "Sree hotel 3",
+				description:
+                " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quo repellat laboriosam illum suscipit minima rem commodi officia expedita a? ",
+				image:
+					"../resources/images/Rectangle 28.png",
+			},
+		],
+	},
+	{
+		id: 3,
+		name: "SUNDARBANS",
+		description:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quo repellat laboriosam illum suscipit minima rem commodi officia expedita a? ",
+		image: "../resources/images/Sundorbon.png",
+		hotels: [
+			{
+				id: 1,
+				name: "Sundarban hotel 1",
+				description:
+                " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quo repellat laboriosam illum suscipit minima rem commodi officia expedita a? ",
+				image:
+					"../resources/images/Rectangle 26.png",
+			},
+			{
+				id: 2,
+				name: "Sundarban hotel 2",
+				description:
+                " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quo repellat laboriosam illum suscipit minima rem commodi officia expedita a? ",
+				image:
+					"../resources/images/Rectangle 27.png",
+			},
+			{
+				id: 3,
+				name: "Sundarban hotel 3",
+				description:
+                " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quo repellat laboriosam illum suscipit minima rem commodi officia expedita a? ",
+				image:
+					"../resources/images/Rectangle 28.png",
+			},
+		],
+	},
+];
+
+export default locations;
